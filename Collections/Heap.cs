1551444a5912a59e8d94ddecb16e87bf92245891
ShootMe <ShootMe>
2022-10-10 15:05:20 -0500

    Heap

--- Collections/Heap.cs
+++ Collections/Heap.cs
@@ -13,48 +13,66 @@ namespace Klondike.Collections {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get { return size; }
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Clear() {
             size = 0;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Enqueue(T node) {
-            int current = size;
-            nodes[size++] = node;
-
+            int current = size++;
             while (current > 0) {
-                int parent = (current - 1) >> 1;
-                if (node.CompareTo(nodes[parent]) >= 0) { break; }
+                int child = current;
+                current = (current - 1) >> 1;
+                T value = nodes[current];
+                if (node.CompareTo(value) >= 0) {
+                    current = child;
+                    break;
+                }
 
-                nodes[current] = nodes[parent];
-                nodes[parent] = node;
-                current = parent;
+                nodes[child] = value;
             }
+
+            nodes[current] = node;
         }
         [MethodImpl(MethodImplOptions.AggressiveOptimization)]
         public T Dequeue() {
             T result = nodes[0];
             int current = 0;
-            nodes[0] = nodes[--size];
+            T end = nodes[--size];
 
             do {
                 int last = current;
-                int left = (current << 1) + 1;
-                int right = left + 1;
-                if (size > left && nodes[current].CompareTo(nodes[left]) > 0) {
-                    current = left;
-                }
-                if (size > right && nodes[current].CompareTo(nodes[right]) > 0) {
-                    current = right;
+                int child = (current << 1) + 1;
+
+                if (size > child && end.CompareTo(nodes[child]) > 0) {
+                    current = child;
+
+                    if (size > ++child && nodes[current].CompareTo(nodes[child]) > 0) {
+                        current = child;
+                    }
+                } else if (size > ++child && end.CompareTo(nodes[child]) > 0) {
+                    current = child;
+                } else {
+                    break;
                 }
 
-                if (current == last) { break; }
-
-                T temp = nodes[current];
-                nodes[current] = nodes[last];
-                nodes[last] = temp;
+                nodes[last] = nodes[current];
             } while (true);
 
+            nodes[current] = end;
+
             return result;
+        }
+        private void Resize() {
+            T[] newNodes = new T[(int)(nodes.Length * 1.5)];
+            for (int i = 0; i < nodes.Length; i++) {
+                newNodes[i] = nodes[i];
+            }
+            nodes = newNodes;
+        }
+
+        public override string ToString() {
+            return $"Count = {Count}";
         }
     }
 }
